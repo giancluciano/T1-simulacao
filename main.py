c1b456587c0f76f021f3dad765fafd9e44d8bcf7
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2018-09-08 16:20:42 -0300

    feat: Add random number generator

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 # tempo_global
 # tabela_eventos
 # simulador
@@ -8,10 +10,30 @@
 agenda = []
 aleatorios = [0.1195, 0.3491, 0.9832]
 eventos = [
-            {'evento': 'chX' | 'saX', 'tempo': 1, 'sorteio': 1},
-          ]
+    # Comentei esta linha, ja que o pipe faz um TypeError ao usar strings
+    # {'evento': 'chX' | 'saX', 'tempo': 1, 'sorteio': 1},
+]
 fila = 0
 tempo_global = 0
+
+
+# variaveis usadas na geracao de numeros aleatorios
+x_anterior = 19
+M = 4493
+a = 500
+c = 4
+quantidade_iteracoes = 0
+
+
+def congruente_linear():
+    global x_anterior, M, c, a, quantidade_iteracoes
+
+    quantidade_iteracoes = quantidade_iteracoes + 1
+    # Resto da divisao em python 3 usamos o %. Se python 2, / direto
+    x_atual = (a * x_anterior + c) % M
+    x_anterior = x_atual
+    # Divisao no python 3 usamos /. Se python 2, precisa fazer um cast para float/Decimal
+    return x_atual / M
 
 
 def agenda_chegada(tempo, sorteio):
@@ -36,10 +58,17 @@ def eventoSA(evento):
     agenda_saida()
 
 
+# Comentado para testar o gerador do numeros aleatorios
+# while True:
+#     next_evento = agenda.pop(0)
+#     tempo_global = next_evento.tempo
+#     if next_evento.evento == 'ch':
+#         eventoCH(next_evento)
+#     else:
+#         eventoSA(next_evento)
+
+
+# Teste do gerador de numero aleatorios
 while True:
-    next_evento = agenda.pop(0)
-    tempo_global = next_evento.tempo
-    if next_evento.evento == 'ch':
-        eventoCH(next_evento)
-    else:
-        eventoSA(next_evento)
+    print(quantidade_iteracoes, ' - ', congruente_linear())
+    sleep(0.01)
