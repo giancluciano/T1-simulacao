8d7433d30bb20e006856b880b4a6f0dba2a245bb
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2018-09-08 15:24:13 -0300

    style: PEP8 adjustments

--- main.py
+++ main.py
@@ -2,32 +2,39 @@
 # tabela_eventos
 # simulador
 
-# start -> gera_1_evento 
+# start -> gera_1_evento
 
 # evento -> 1 ou mais eventos e start no mais proximo
 agenda = []
 aleatorios = [0.1195, 0.3491, 0.9832]
 eventos = [
-            {'evento':'chX'|'saX', 'tempo':1, 'sorteio':1},
+            {'evento': 'chX' | 'saX', 'tempo': 1, 'sorteio': 1},
           ]
 fila = 0
 tempo_global = 0
 
+
 def agenda_chegada(tempo, sorteio):
-    agenda.append({'evento':'ch','tempo':tempo+sorteio,'sorteio':sorteio})
+    agenda.append({'evento': 'ch', 'tempo': tempo + sorteio, 'sorteio': sorteio})
+
 
 def agenda_saida(tempo, sorteio):
-    agenda.append({'evento':'sa','tempo':tempo+sorteio,'sorteio':sorteio})
+    agenda.append({'evento': 'sa', 'tempo': tempo + sorteio, 'sorteio': sorteio})
+
 
 def start():
     agenda_chegada(0, 2.5)
 
+
 def eventoCH(evento):
-    eventos.append({'evento':'ch','fila':fila,'tempo':tempo_global}) # + tempo em cada estado da fila
+    eventos.append({'evento': 'ch', 'fila': fila, 'tempo': tempo_global})  # + tempo em cada estado da fila
     agenda_chegada()
+
+
 def eventoSA(evento):
-    eventos.append({'evento':'sa','fila':fila,'tempo':tempo_global}) # + tempo em cada estado da fila
+    eventos.append({'evento': 'sa', 'fila': fila, 'tempo': tempo_global})  # + tempo em cada estado da fila
     agenda_saida()
+
 
 while True:
     next_evento = agenda.pop(0)
